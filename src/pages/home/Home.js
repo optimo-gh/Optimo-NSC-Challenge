2ea2bbb165cdb9ca22830715f4ce1e0ad5b29eee
C L <happycindy26@gmail.com>
2021-08-21 19:23:33 +1000

    added pages

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Are you a fresh worker who want to decide your first job or you want a career transtion?</h1>
+            <button>Look for first job</button>
+            <button>Career transition</button>
+        </div>
+    )
+}
+
+export default Home;
